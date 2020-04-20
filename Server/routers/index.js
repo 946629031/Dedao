f7d10734eb35b931069be15043963b375dc9c540
threeki <946629031@qq.com>
2020-04-20 17:55:39 +0800

    fixed 读取文件时 判断文件是否存在

--- Server/routers/index.js
+++ Server/routers/index.js
@@ -23,7 +23,7 @@ router.get('/uploadFile/:filename', async(ctx, next) => {
 	var mp3 = path.resolve(__dirname + '../../../../uploadFile/' + ctx.params.filename)
 
 	fs.exists(mp3, function(exists) {
-		console.log(exists ? "文件存在" : "文件不存在")
+		console.log(mp3, exists ? "文件存在" : "文件不存在")
 		if (exists) {
 			// stream file
 			const src = fs.createReadStream(mp3)
